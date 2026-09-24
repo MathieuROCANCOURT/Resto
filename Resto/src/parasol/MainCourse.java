dd15a2a1ca4d58d75d609849f12cbc828c9f1a44
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-24 09:20:27 +0200

    feat: Add constructor in MainCourse class.

--- Resto/src/parasol/MainCourse.java
+++ Resto/src/parasol/MainCourse.java
@@ -6,6 +6,12 @@ package parasol;
 /**
  * 
  */
-public class MainCourse extends Menu {
-
+public class MainCourse extends MenuOption {
+	/**
+	 * @param option
+	 * @param price
+	 */
+	public MainCourse(String option, float price) {
+		super(option, price);
+	}
 }
