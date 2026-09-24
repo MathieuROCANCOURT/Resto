3fbfe15e532c035dffb9e34587df03f5c6a0b124
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-24 09:18:48 +0200

    feat: Add MenuOption class with constructor.

---
+++ Resto/src/parasol/MenuOption.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package parasol;
+
+/**
+ * 
+ */
+protected class MenuOption {
+	private String option;
+	private float price;
+	
+	/**
+	 * @param option
+	 * @param price
+	 */
+	public MenuOption(String option, float price) {
+		this.option = option;
+		this.price = price;
+	}
+	
+	
+}
