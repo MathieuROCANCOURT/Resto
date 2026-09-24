7ac14d899f819d43d920c58ff72af30ba6189032
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-24 13:01:21 +0200

    feat: Add getters and complete add MenuOption method in UserCommand class.

--- Resto/src/parasol/UserCommand.java
+++ Resto/src/parasol/UserCommand.java
@@ -13,7 +13,16 @@ public class UserCommand {
 	private List<MenuOption> menuUser = new ArrayList<MenuOption>();
 	private float totalCommand = 0f;
 	
+	public List<MenuOption> getMenuUser() {
+		return menuUser;
+	}
+
+	public float getTotalCommand() {
+		return totalCommand;
+	}
+
 	public void addMenuOption(MenuOption option) {
 		this.menuUser.add(option);
+		this.totalCommand += option.getPrice();
 	}
 }
