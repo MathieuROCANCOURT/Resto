464622b555bb4c1e5fdf786ae573037fd0a3834c
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-24 12:58:47 +0200

    feat: Add addMenuOption in UserCommand class.

--- Resto/src/parasol/UserCommand.java
+++ Resto/src/parasol/UserCommand.java
@@ -3,9 +3,17 @@
  */
 package parasol;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  */
 public class UserCommand {
-
+	private List<MenuOption> menuUser = new ArrayList<MenuOption>();
+	private float totalCommand = 0f;
+	
+	public void addMenuOption(MenuOption option) {
+		this.menuUser.add(option);
+	}
 }
