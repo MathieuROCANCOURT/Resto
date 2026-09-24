cd60fcec5390f3a9fba76b416e023a850ceb43dd
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-24 11:19:48 +0200

    feat: Add main method and adapt with others class.

--- Resto/src/parasol/Main.java
+++ Resto/src/parasol/Main.java
@@ -3,6 +3,16 @@
  */
 package parasol;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * 
  */
@@ -12,7 +22,74 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean isNumberAndPos = false;
+		int nbCustomer = 0;
 
+		while (!isNumberAndPos) {
+			System.out.print("Bonjour ! Combien de manu(s) souhaitez-vous ?");
+			if (sc.hasNextInt()) {
+				nbCustomer = sc.nextInt();
+				if (nbCustomer > 0) {
+					isNumberAndPos = true;
+				} else {
+					System.err.println("La valeur doit être positif");
+
+				}
+			} else {
+				System.err.println("La valeur a saisir doit être un entier positif.");
+			}
+		}
+
+		Menu menuCard = new Menu();
+		
+		ArrayList<List<MenuOption>> allMenu = new ArrayList<List<MenuOption>>();
+		for (int person = 1; person <= nbCustomer; person++) {
+			System.out.println("Commande numéro " + person);
+			List<MenuOption> menu = menuCard.defineMenu();
+			System.out.println("Résumé de la commande " + person);
+			System.out.println(menu + "\n");
+			allMenu.add(menu);
+		}
+
+		System.out.println("----------------Voici le récapitulatifs des menus-----------------");
+		int nbPerson = 0;
+		for (List<MenuOption> menu : allMenu) {
+			nbPerson += 1;
+			System.out.println("Menu " + nbPerson + ": " + menu);
+		}
+		sc.close();
+		
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(new File("order.txt"));
+			int nbMenu = 1;
+			
+			for (List<MenuOption> menu : allMenu) {
+				String header = "*******************Résumé de la commande n°" + nbMenu + " *******************\n";
+				fos.write(header.getBytes());
+				
+				for (MenuOption choice: menu) {
+					fos.write(choice.getOption().getBytes());
+					fos.write("\n".getBytes());
+				}
+				fos.write("\n\n".getBytes());
+				nbMenu++;
+			}
+			
+		} catch (FileNotFoundException e) {
+			Logger logger = Logger.getAnonymousLogger();
+			logger.log(Level.SEVERE, e.getLocalizedMessage());
+		} catch (IOException e) {
+			Logger logger = Logger.getAnonymousLogger();
+			logger.log(Level.SEVERE, e.getLocalizedMessage());
+		}finally {
+			try {
+				if (fos != null) fos.close();
+			} catch (IOException e) {
+				Logger logger = Logger.getAnonymousLogger();
+				logger.log(Level.SEVERE, e.getLocalizedMessage());
+			}
+		}
 	}
-
 }
