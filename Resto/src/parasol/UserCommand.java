5397fd8a6226a0876adf2658b41cc78c1b19cd89
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-24 14:19:14 +0200

    chore: Remove display choice if is nothing in UserCommand.

--- Resto/src/parasol/UserCommand.java
+++ Resto/src/parasol/UserCommand.java
@@ -11,15 +11,15 @@ import java.util.List;
  */
 public class UserCommand {
 	private static int counter = 0;
-	
+
 	private final int id;
 	private List<MenuOption> menuUser = new ArrayList<MenuOption>();
 	private float totalCommand = 0f;
-	
+
 	public UserCommand() {
 		this.id = ++counter;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
@@ -29,24 +29,28 @@ public class UserCommand {
 	}
 
 	public float getTotalCommand() {
-		return Integer.parseInt(String.format("%.02f", this.totalCommand));
+		return Float.parseFloat(String.format("%.02f", this.totalCommand));
 	}
 
 	public void addMenuOption(MenuOption option) {
-		this.menuUser.add(option);
-		this.totalCommand += option.getPrice();
+		if (!option.getOption().startsWith("AUCUN")) {
+			this.menuUser.add(option);
+			this.totalCommand += option.getPrice();
+		}
 	}
 
 	@Override
 	public String toString() {
 		StringBuilder resume = new StringBuilder("Menu n°" + this.id + ":");
-		for (MenuOption option: this.menuUser) {
-			resume.append(option.getOption().toLowerCase());
-			resume.append(", ");
+		for (MenuOption option : this.menuUser) {
+			if (!option.getOption().startsWith("AUCUN")) {
+				resume.append(option.getOption().toLowerCase());
+				resume.append(", ");
+			}
 		}
 		String strTotalCommand = String.format("%.02f", this.totalCommand);
 		resume.append("Prix total: " + strTotalCommand + "€.");
-		
+
 		return resume.toString();
 	}
 }
