f9eff6cf479c69893ae1c5b5c03bfadcbc619b2c
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-24 09:06:40 +0200

    feat: Add constructor and attributes in Appetizer class.

--- Resto/src/parasol/Appetizer.java
+++ Resto/src/parasol/Appetizer.java
@@ -6,6 +6,18 @@ package parasol;
 /**
  * 
  */
-public class Appetizer extends Menu {
-
+public class Appetizer {
+	private String appetizer;
+	private float price;
+	
+	/**
+	 * @param appetizer
+	 * @param price
+	 */
+	public Appetizer(String appetizer, float price) {
+		this.appetizer = appetizer;
+		this.price = price;
+	}
+	
+	
 }
