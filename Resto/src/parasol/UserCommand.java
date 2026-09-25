27c2639daf4de9dfe0ad734ad3c56b529cbad6d8
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-25 13:25:27 +0200

    docs: Add all docstrings in UserCommand class.

--- Resto/src/parasol/UserCommand.java
+++ Resto/src/parasol/UserCommand.java
@@ -16,22 +16,46 @@ public class UserCommand {
 	private List<MenuOption> menuUser = new ArrayList<MenuOption>();
 	private float totalCommand = 0f;
 
+	/**
+	 * Create a UserCommand where id is equal to the number of instantiate before
+	 * him.
+	 */
 	public UserCommand() {
 		this.id = ++counter;
 	}
 
+	/**
+	 * Get the UserCommand id.
+	 * 
+	 * @return id
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Get the menu user choices.
+	 * 
+	 * @return Menu user
+	 */
 	public List<MenuOption> getMenuUser() {
 		return menuUser;
 	}
 
+	/**
+	 * Get the price command total by user.
+	 * 
+	 * @return Price command total
+	 */
 	public float getTotalCommand() {
 		return Float.parseFloat(String.format("%.02f", this.totalCommand));
 	}
 
+	/**
+	 * Add the customer's selection to the customer menu.
+	 * 
+	 * @param option The customer selection
+	 */
 	public void addMenuOption(MenuOption option) {
 		if (!option.getOption().startsWith("AUCUN")) {
 			this.menuUser.add(option);
