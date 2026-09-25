c0cd7beff20320128379a9d725ce72fc2acda705
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-25 13:14:38 +0200

    docs: Add docstring to Drink class.

--- Resto/src/parasol/Drink.java
+++ Resto/src/parasol/Drink.java
@@ -4,7 +4,9 @@
 package parasol;
 
 /**
+ * The class can implemented a drink choice in the menu with associated price.
  * 
+ * @author RocancourtM
  */
 public class Drink extends MenuOption {
 	/**
