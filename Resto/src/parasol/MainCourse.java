a543d575193f99c6d60f39ca0aaff6f60af5a480
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-25 13:07:13 +0200

    docs: Add docstring to constructor in MainCourse class.

--- Resto/src/parasol/MainCourse.java
+++ Resto/src/parasol/MainCourse.java
@@ -8,8 +8,8 @@ package parasol;
  */
 public class MainCourse extends MenuOption {
 	/**
-	 * @param mainCourse
-	 * @param price
+	 * @param mainCourse Main course name.
+	 * @param price Main course price.
 	 */
 	public MainCourse(String mainCourse, float price) {
 		super(mainCourse, price);
