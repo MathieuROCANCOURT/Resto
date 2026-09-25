c1b210aa6c52b2d6229fd2451b66be8e799b1ccb
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-25 13:07:32 +0200

    docs: Add docstring to constructor in SideDish class.

--- Resto/src/parasol/SideDish.java
+++ Resto/src/parasol/SideDish.java
@@ -8,8 +8,8 @@ package parasol;
  */
 public class SideDish extends MenuOption {
 	/**
-	 * @param sideDish
-	 * @param price
+	 * @param sideDish Side dish name.
+	 * @param price side dish price.
 	 */
 	public SideDish(String sideDish, float price) {
 		super(sideDish, price);
