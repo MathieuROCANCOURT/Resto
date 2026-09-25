c377a661666a810c02b565c98cd5bba09b9c5884
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-25 13:06:57 +0200

    docs: Add docstring to constructor in Appetizer class.

--- Resto/src/parasol/Appetizer.java
+++ Resto/src/parasol/Appetizer.java
@@ -8,8 +8,8 @@ package parasol;
  */
 public class Appetizer extends MenuOption {	
 	/**
-	 * @param appetizer
-	 * @param price
+	 * @param appetizer Appetizer name.
+	 * @param price Appetizer price.
 	 */
 	public Appetizer(String appetizer, float price) {
 		super(appetizer, price);
